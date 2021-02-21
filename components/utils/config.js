ab7ea35484151ab4e986d6d988b78c9f6b329bf6
aratius <arata1129matsu@icloud.com>
2021-02-21 22:37:30 +0900

    やっぱりdotenvファイル大公開

--- components/utils/config.js
+++ components/utils/config.js
@@ -1,6 +1,6 @@
 export const API = {
   WORKS: {
-    GET: 'https://aualrxse.microcms.io/api/v1/works',
-    KEY: '1fefa953-658c-4c55-96b3-302aaf5c3dd4'
+    GET: process.env.API_ENDPOINT,
+    KEY: process.env.API_KEY
   }
 }
