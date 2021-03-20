05f2f57bd0a249a68df1e0c143b4800a595d3adf
aratius <arata1129matsu@icloud.com>
2021-03-20 14:35:16 +0900

    魚の画像を追加

--- components/react/common/header.js
+++ components/react/common/header.js
@@ -11,10 +11,14 @@ export default class Header extends React.Component {
         <div className={styles.container}>
           <TransitionLink href="/"><img src="/assets/images/logos/cocoon.svg"/></TransitionLink>
           <ul>
-            <li><TransitionLink href="/works"><img src="/assets/images/logos/works.svg"/></TransitionLink></li>
+            {/* <li><TransitionLink href="/works"><img src="/assets/images/logos/works.svg"/></TransitionLink></li>
             <li><TransitionLink href="/about"><img src="/assets/images/logos/about.svg"/></TransitionLink></li>
             <li><Link href=""><img src="/assets/images/logos/contact.svg"/></Link></li>
-            <li><Link href=""><img src="/assets/images/logos/blog.svg"/></Link></li>
+            <li><Link href=""><img src="/assets/images/logos/blog.svg"/></Link></li> */}
+            <li><TransitionLink href="/works">link</TransitionLink></li>
+            <li><TransitionLink href="/about">link</TransitionLink></li>
+            <li><Link href="">link</Link></li>
+            <li><Link href="">link</Link></li>
           </ul>
         </div>
       </>
--- components/utils/tweenManager.js
+++ components/utils/tweenManager.js
@@ -6,7 +6,7 @@ export default class TweenManager {
   static popUp(element, duration=0.3, delay=0, ease="elastic.out") {
     if(!element) return
     element.style.zIndex = 100;
-    gsap.timeline().to(element, {scale: 1.2, duration: duration, delay: delay, ease: ease})
+    gsap.timeline().fromTo(element,{scale: 1}, {scale: 1.2, duration: duration, delay: delay, ease: ease})
   }
 
   static popDown(element, duration=0.3, delay=0, ease="elastic.out") {
