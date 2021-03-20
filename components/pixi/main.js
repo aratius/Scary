b01e8f07390982477e584a69736d5de08ea39684
aratius <arata1129matsu@icloud.com>
2021-03-20 18:23:39 +0900

    2回マウントを走らないように

--- components/pixi/main.js
+++ components/pixi/main.js
@@ -1,19 +1,24 @@
-import { createRef, useEffect } from "react"
+import { createRef, useEffect, useState } from "react"
 import * as PIXI from 'pixi.js'
 import { Application } from 'pixi.js'
 
 export default function Pure() {
 
   const stage = createRef(null)
+  const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
+    if(mounted) return
+
     const app = new Application({
       width: window.innerWidth,
-      height: window.innerHeight,
+      height: document.documentElement.scrollHeight,
       // backgroundAlpha: 0,
-      backgroundColor: 0xf00000
+      // backgroundColor: 0xf00000
     })
     stage.current.appendChild(app.view)
+
+    setMounted(true)
   }, [])
 
   return (
