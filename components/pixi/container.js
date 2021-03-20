7e65ae4fbb1de8494dd081b7f37a54ab3db49381
aratius <arata1129matsu@icloud.com>
2021-03-20 19:06:22 +0900

    container用意

--- components/pixi/container.js
+++ components/pixi/container.js
@@ -1,19 +1,22 @@
 import * as PIXI from 'pixi.js'
-import { Container } from 'pixi.js'
+import { Container, Sprite, Texture } from 'pixi.js'
+import Vector2 from './common/vector2'
 
-export default class Container extends Container {
+export default class myContainer extends Container {
 
   constructor(){
     super()
   }
 
-  init() {
-    this.update()
+  Init() {
+    const texture = new Texture.from("/assets/images/pixi/fishes/fish_1.png")
+    this.sprite = new Sprite(texture)
+    this.sprite.position.set(100, 100)
+    this.addChild(this.sprite)
   }
 
-  update() {
-    requestAnimationFrame(this.update)
-    console.log('update')
+  Update() {
+
   }
 
 }
--- components/pixi/main.js
+++ components/pixi/main.js
@@ -1,10 +1,11 @@
 import { createRef, useEffect, useState } from "react"
 import * as PIXI from 'pixi.js'
 import { Application } from 'pixi.js'
+import myContainer from './container'
 
 export default function Pure() {
 
-  const stage = createRef(null)
+  const targetDOM = createRef(null)
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
@@ -17,11 +18,20 @@ export default function Pure() {
       backgroundAlpha: 0.3,
       backgroundColor: 0xf0a000
     })
-    stage.current.appendChild(app.view)
+    targetDOM.current.appendChild(app.view)
+
+    const container = new myContainer()
+    container.Init()
+    const update = () => {
+      requestAnimationFrame(update)
+      container.Update()
+    }
+    update()
+    app.stage.addChild(container)
   }, [])
 
   return (
-    <div id="pure" ref={stage}></div>
+    <div id="pure" ref={targetDOM}></div>
   )
 
 }
