029d5dd57fc0de12aa5d10587f7cbe850f4f625f
aratius <arata1129matsu@icloud.com>
2021-04-08 01:27:06 +0900

    タイトル変更

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home (props) {
     >
       <div className={styles.lead__container}>
         {/* <img src="/assets/images/logos/cocoon.svg"/> */}
-        <h1>title</h1>
+        <h1>制作中のポートフォリオサイト</h1>
       </div>
       <div className={styles.main__container}>
         <_Works data={data} styles={STYLES_WORKS.TOP}/>
