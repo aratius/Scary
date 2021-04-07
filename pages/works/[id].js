0a0f070f4d93f0a0270df96503e5e9de680902ef
aratius <arata1129matsu@icloud.com>
2021-04-08 01:54:05 +0900

    ポートフォリオの詳細ページを雑に作成

--- pages/works/[id].js
+++ pages/works/[id].js
@@ -2,11 +2,13 @@ import { useRouter } from 'next/router'
 import { getWorks } from '../../components/api/works'
 import Base from '../../components/react/base'
 import baseStyles from '../../styles/modules/common/base.module.scss'
+import WorkDetailStyles from '../../styles/modules/works/detail.module.scss'
 
 export default function Work (props) {
 
   const router = useRouter()
   const works = props.works
+  console.log(works);
 
   const randomPos = {x: Math.random() * 700, y: Math.random() * 700}
 
@@ -17,7 +19,18 @@ export default function Work (props) {
       title="WORKS"
     >
       <div className={baseStyles.main__container}>
-        {works.title}
+        <h1>{works.title}</h1>
+        <img src={works.main_image.url}></img>
+        <hr/>
+        <p>{works.description}</p>
+        <hr/>
+        <div className={WorkDetailStyles.subimage_container}>
+          {works.subimages && works.subimages.map((img ,key) => {
+            return (
+              <img src={img.subimage.url} key={key} />
+            )
+          })}
+        </div>
       </div>
     </Base>
     </>
