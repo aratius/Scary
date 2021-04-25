2a8d6a1cc839fe89f5536978949af164aaaa8cd5
aratius <arata1129matsu@icloud.com>
2021-04-25 20:52:13 +0900

    work detailのスタイルを軽く作成

--- pages/works/[id].js
+++ pages/works/[id].js
@@ -12,6 +12,23 @@ export default function Work (props) {
 
   const randomPos = {x: Math.random() * 700, y: Math.random() * 700}
 
+  const description = (sentence) => {
+    return (
+      <p className={WorkDetailStyles.description__wrapper}>
+        {sentence.split('\n').map((data, key) => {
+          return (
+            <React.Fragment key={key}>
+              <span className={WorkDetailStyles.description__detail}>{data}</span>
+              <br/>
+            </React.Fragment>
+          )
+        })}
+      </p>
+    )
+  }
+
+  console.log(works);
+
   return (
     <>
     <Base
@@ -21,6 +38,7 @@ export default function Work (props) {
       <div className={baseStyles.main__container}>
         <h2>{works.title}</h2>
         <div className={WorkDetailStyles.utils__container}>
+          {/* 使用技術 */}
           {works.utils && works.utils.map((util, key) => {
             return(
               <span className={WorkDetailStyles.utils__detail} key={key}>
@@ -29,23 +47,40 @@ export default function Work (props) {
             )
           })}
         </div>
+        {/* でっかいメイン画像 */}
         <img src={works.main_image.url} className={WorkDetailStyles.main__image}></img>
         <hr/>
-        <p className={WorkDetailStyles.description__wrapper}>
-          {works.description && works.description.split('\n').map((data, key) => {
-            return (
-              <React.Fragment key={key}>
-                <span className={WorkDetailStyles.description__detail}>{data}</span>
-                <br/>
-              </React.Fragment>
-            )
-          })}
-        </p>
+        {works.description  && description(works.description)}
         <hr/>
-        <div className={WorkDetailStyles.subimage_container}>
-          {works.subimages && works.subimages.map((img ,key) => {
+
+        {/*
+          サブ画像とサブ説明
+          ここのコードの気持ち悪さ半端ない
+          システム構成見直す
+        */}
+        <div className={WorkDetailStyles.subimage__container}>
+          {works.subimages && works.subimages.map((img ,i) => {
+            const subimageStyle = i == 1 ? WorkDetailStyles.img_normal : WorkDetailStyles.img__small;
             return (
-              <img src={img.subimage.url} key={key} />
+              <>
+                <img src={img.subimage.url} className={subimageStyle} key={i} />
+                {i==0 && works.sub_description_1 &&
+                  <>
+                    <hr/>
+                      {description(works.sub_description_1)}
+                    <hr/>
+                  </>
+                }
+                {i==1 && works.sub_description_2 &&
+                  <>
+                    <hr/>
+                      {description(works.sub_description_2)}
+                    <hr/>
+                  </>
+                }
+                {i == 1 && <hr/>}
+
+              </>
             )
           })}
         </div>
