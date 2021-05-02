a63789e56a205cbe9aad366a965c9308fb455dab
aratius <arata1129matsu@icloud.com>
2021-05-02 22:21:44 +0900

    一箇所ボタン消す

--- pages/works/[id].tsx
+++ pages/works/[id].tsx
@@ -25,24 +25,25 @@ interface Props {
 
 const Work: React.FC<Props> = (props) => {
 
-  const router = useRouter()
   const works = props.works
-  const descriptions = works.description.split("<hr>")
-  console.log(descriptions);
+  console.log(works.description)
 
+  const descriptions = works.description.split("<hr>")
 
   const randomPos = {x: Math.random() * 700, y: Math.random() * 700}
 
   const description = (sentence) => {
+
     return (
       <p className={WorkDetailStyles.description__wrapper}>
-        {sentence.length && sentence.split('\n').map((data, i) => {
+        {/* {sentence.length && sentence.split('\n').map((data, i) => {
           return (
             <React.Fragment key={i}>
               <span className={WorkDetailStyles.description__detail}>{data}</span>
             </React.Fragment>
           )
-        })}
+        })} */}
+        arata
       </p>
     )
   }
