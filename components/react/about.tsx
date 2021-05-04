9766966fa15c41abba8dbf91d0f1b80ff57b6bd0
aratius <arata1129matsu@icloud.com>
2021-05-04 21:28:01 +0900

    aboutページをうっすら実装

--- components/react/about.tsx
+++ components/react/about.tsx
@@ -1,4 +1,14 @@
+import React from 'react'
 import baseStyles from '../../styles/modules/common/base.module.scss'
+import aboutStyles from '../../styles/modules/about/index.module.scss'
+import gsap from "gsap"
+// SSRモード（サーバー上）では使えないためこの条件分岐
+const ScrollTrigger = process.browser ? require("gsap/ScrollTrigger") : undefined
+process.browser && gsap.registerPlugin(ScrollTrigger)
+
+// SSRモード（サーバー上）では使えないためこの条件分岐
+const ScrollToPlugin = process.browser ? require("gsap/ScrollToPlugin") : undefined
+process.browser && gsap.registerPlugin(ScrollToPlugin)
 
 interface Props {
   data: {
@@ -9,47 +19,99 @@ interface Props {
   }
 }
 
-const About: React.FC<Props> = ({data}) => {
+class About extends React.Component {
 
-  return (
+  blocks: HTMLElement[]
+
+  constructor(props) {
+    super(props)
+
+    this.blocks = []
+  }
+
+  // タイトルが見えたときになんかする
+  onReadyTitle (node) {
+    gsap.to(node, {
+      scrollTrigger: node,
+
+    })
+  }
+
+  componentDidMount() {
+    if(this.blocks.length == 0) return
+
+
+    for(const i in this.blocks) {
+      gsap.to(this.blocks[i], {
+        scrollTrigger: {
+          trigger: this.blocks[i],
+          start: "top bottom",
+          markers: true,
+          onEnter: () => {
+            window.addEventListener("scroll", this.handleScroll, {passive: false})
+            gsap.to(window, {scrollTo: this.blocks[i], duration: 2, ease: "circ.inOut", onComplete: () => {
+              window.removeEventListener("scroll", this.handleScroll)
+            }})
+          },
+          onLeaveBack: () => {
+          }
+        },
+      })
+
+    }
+
+  }
+
+  handleScroll (e) {
+    console.log("he");
+
+    e.preventDefault()
+  }
+
+  render() {
+
+    return (
       <>
 
+        <div className={aboutStyles.info__block} ref={node => this.blocks[0] = node}>
+          <h2>arata matsumoto</h2>
+          <br/>
+        </div>
 
-        <h2>{data.name}</h2>
-        <br/>
+        <div className={aboutStyles.info__block} ref={node => this.blocks[1] = node}>
+          <h3 ref={this.onReadyTitle}>about me</h3>
+          <p>・ 2000.11.29 (20), born in Hyogo</p>
+          <p>・ front end developer / backpacker</p>
+          <p>・ STARRYWORKS inc. ( full time )</p>
+          <p>・ osaka univercity of arts ( currently attending )</p>
+          <br/>
+        </div>
 
-        <p>・ STARRYWORKS inc. ( full time )</p>
-        <p>・ front end developer / backpacker</p>
-        <p>・ 2000.11.29, born in Hyogo</p>
-        <p>・ osaka univercity of arts</p>
-        <br/>
-
-        <div>
-          <h2>lang</h2>
+        <div className={aboutStyles.info__block} ref={node => this.blocks[2] = node}>
+          <h3 ref={this.onReadyTitle}>lang</h3>
           <br/>
 
-          <h3>javascript</h3>
+          <h4>javascript</h4>
           <p>・ React.js</p>
           <p>・ Next.js</p>
           <p>・ pixi.js</p>
           <p>・ three.js</p>
           <br/>
 
-          <h3>C++</h3>
+          <h4>C++</h4>
           <p>・ openframeworks</p>
           <p>・ GLSL</p>
           <p>・ Arduino</p>
           <br/>
 
-          <h3>C#</h3>
+          <h4>C#</h4>
           <p>・ Unity</p>
-
+          <br/>
         </div>
 
-
       </>
-  )
-
+    )
+  }
 }
 
 export default About
