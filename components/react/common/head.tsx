acfba7714653a2c324fe64154dbbe4fd93a1d4c2
aratius <arata1129matsu@icloud.com>
2021-05-05 14:52:54 +0900

    もう一回og

--- components/react/common/head.tsx
+++ components/react/common/head.tsx
@@ -13,13 +13,13 @@ const _Head: React.FC<Props> = ({title}) => {
       <link rel="preconnect" href="https://fonts.gstatic.com" />
 
       {/* OG */}
-      <meta property="og:url" content="https://scary.aualrxse.com" />
+      <meta property="og:url" content="https://scary.aualrxse.com/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="aualrxse" />
       <meta property="og:description" content="front end developer" />
       <meta property="og:site_name" content="arata matsumoto" />
-      <meta property="og:image" content="/assets/images/og-image.jpg" />
-      <meta name="twitter:card" content="" />
+      <meta property="og:image" content="https://scary.aualrxse.com/assets/images/og-image.jpg" />
+      {/* <meta name="twitter:card" content="" /> */}
       <meta name="twitter:site" content="@aualrxse" />
     </Head>
   )
