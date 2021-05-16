81ea03b0f524b36c860c58e0d0778aa74b47854d
aratius <arata1129matsu@icloud.com>
2021-05-16 18:26:37 +0900

    scroll終了時の処理

--- components/react/top/titleList.tsx
+++ components/react/top/titleList.tsx
@@ -15,15 +15,17 @@ export default class TitleList extends React.Component<Props> {
   titles: HTMLElement[]
   titlePositions: number[]
   activeElementData: any
-  topItem: any
-  bottomItem: any
   updater: any
+  scrollEndTimer: any
+  scrolling: boolean
 
   constructor(props) {
     super(props)
     this.titles = []
     this.titlePositions = []
     this.activeElementData
+    this.scrollEndTimer
+    this.scrolling = false
   }
 
   componentDidMount() {
@@ -36,6 +38,8 @@ export default class TitleList extends React.Component<Props> {
 
   update = ():void => {
     const topThresold = this.activeElementData.top - this.activeElementData.height * 3
+    const bottomThreshold = topThresold + this.activeElementData.height * (this.titles.length+1)
+    const sumItemHeight = this.activeElementData.height * this.titles.length
 
     if(this.titles.length > 0) {
       for(const i in this.titles) {
@@ -49,15 +53,20 @@ export default class TitleList extends React.Component<Props> {
           this.titles[i].classList.remove(styles.activeItem)
         }
 
-        // 上に消えて下に追加
         if(rect.top < topThresold) {
-          gsap.set(this.titles[i], {y: this.titles[this.titles.length-1].getBoundingClientRect().bottom - 70})  // NOTE: 70 = コンテナのpaddingTop
+          // 上に消えて下に追加
+          gsap.set(this.titles[i], {y: `+=${sumItemHeight}`})  // NOTE: 70 = コンテナのpaddingTop
+        }else if (rect.bottom > bottomThreshold) {
+          // 下に消えて上に追加
+          gsap.set(this.titles[i], {y: `-=${sumItemHeight}`})  // NOTE: 70 = コンテナのpaddingTop
         }
 
-        const newrect = this.titles[i].getBoundingClientRect();
-        const newdist = Math.abs(newrect.top - this.activeElementData.top)
-        const alpha = 1 - (newdist / 80)
-        gsap.set(this.titles[i], { alpha: alpha })  // 文字列で+=100と書くと+=現在からの相対移動が可能
+        if(this.scrolling) {
+          const newrect = this.titles[i].getBoundingClientRect();
+          const newdist = Math.abs(newrect.top - this.activeElementData.top)
+          const alpha = 1 - (newdist / 80)
+          gsap.set(this.titles[i], { alpha: alpha })  // 文字列で+=100と書くと+=現在からの相対移動が可能
+        }
 
       }
 
@@ -69,10 +78,33 @@ export default class TitleList extends React.Component<Props> {
   handleScroll = (e) => {
     // if(e && e.cancelable) e.preventDefault();
     // 親でpreventDefaultしているのでしなくて良い
+    this.scrolling = true
     if(this.titles.length == 0) return
 
     for(const i in this.titles) {
-      gsap.set(this.titles[i], {y: `+=${e.deltaY/2}`})  // 文字列で+=100と書くと+=現在からの相対移動が可能
+      gsap.set(this.titles[i], {y: `+=${-e.deltaY/2}`})  // 文字列で+=100と書くと+=現在からの相対移動が可能
+    }
+    clearTimeout(this.scrollEndTimer)
+    this.scrollEndTimer = setTimeout(this.handleScrollEnd, 100)
+  }
+
+  handleScrollEnd = () => {
+    this.scrolling = false
+    if(this.titles.length > 0) {
+      for(const i in this.titles) {
+        const rect = this.titles[i].getBoundingClientRect();
+        const dist = Math.abs(rect.top - this.activeElementData.top)
+        const alpha = 1 - (dist / 80)
+
+        // 自身の高さの半分より近いものをもっともactiveに近い要素としてクラスを付与
+        if(dist < rect.height / 2) {
+          gsap.to(this.titles[i], {alpha: 1, duration: 0.2})
+        } else {
+          gsap.to(this.titles[i], {alpha: alpha/3, duration: 0.5})
+        }
+
+      }
+
     }
   }
 
@@ -85,6 +117,17 @@ export default class TitleList extends React.Component<Props> {
     }
   }
 
+  handleClickItem = (e) => {
+    this.scrolling = true
+    if(e && e.cancelable) e.preventDefault()
+    const el = e.target
+    if (el.style.opacity < 0) return  // opacity的に見えてない状態ならクリック不可能とする
+    const offset = this.activeElementData.top - el.getBoundingClientRect().top
+    for(const i in this.titles) {
+      gsap.to(this.titles[i], {y: `+=${offset}`, duration: 0.4, ease: "circ.out", onComplete: this.handleScrollEnd})
+    }
+  }
+
   render () {
     const works = this.props.works.contents
 
@@ -92,7 +135,7 @@ export default class TitleList extends React.Component<Props> {
       <ul className={styles.container} onWheel={this.handleScroll}>
         {works.map((data, i) => {
           return (
-            <li key={i} className={styles.item} ref={node => this.handleReadyItem(node, i)}>
+            <li key={i} className={styles.item} onClick={this.handleClickItem} ref={node => this.handleReadyItem(node, i)}>
               {data.title.toUpperCase()}
             </li>
           )
